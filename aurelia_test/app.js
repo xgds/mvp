f0e5fd0ce4ecd4fd227d29eceb4a0d72d820651a
Tamar Cohen <tamar.cohen@nasa.gov>
2018-11-15 13:43:47 -0800

    added comments to docker-compose.yml

---
+++ aurelia_test/app.js
@@ -0,0 +1,8 @@
+import 'bootstrap';
+
+export class App {
+  constructor() {
+  
+
+  }
+}
---
+++ aurelia_test/main.js
@@ -0,0 +1,25 @@
+import environment from './environment';
+import 'bootstrap';
+
+//Configure Bluebird Promises.
+Promise.config({
+  warnings: {
+    wForgottenReturn: false
+  }
+});
+
+export function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
+  aurelia.start().then(() => aurelia.setRoot());
+}
